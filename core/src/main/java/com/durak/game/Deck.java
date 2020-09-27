438dd94b6d4e183f2e7b3f780e4ee5960ffcbb83
mazurmaksim <maksimka37@gmail.com>
2020-09-27 11:52:41 +0300

    поправил класс колода, теперь карты добавляеться циклом а значения берутся из двух енумов.

--- core/src/main/java/com/durak/game/Deck.java
+++ core/src/main/java/com/durak/game/Deck.java
@@ -7,110 +7,117 @@ import com.durak.game.entities.Entity;
 
 public class Deck extends Entity {
 
-	private Game game;
-	public LinkedList<Card> deck;
-	private float x, y;
+    private Game game;
+    public LinkedList<Card> deck;
+    private float x, y;
 
-	public float getX() {
-		return x;
-	}
+    public float getX() {
+        return x;
+    }
 
-	public void setX(float x) {
-		this.x = x;
-	}
+    public void setX(float x) {
+        this.x = x;
+    }
 
-	public float getY() {
-		return y;
-	}
+    public float getY() {
+        return y;
+    }
 
-	public void setY(float y) {
-		this.y = y;
-	}
+    public void setY(float y) {
+        this.y = y;
+    }
 
-	public Deck(Game game, float x, float y) {
+    public Deck(Game game, float x, float y) {
 
-		super(x, y);
-		this.x = x;
-		this.y = y;
-		this.game = game;
+        super(x, y);
+        this.x = x;
+        this.y = y;
+        this.game = game;
 
-	}
+    }
 
-	public int size() {
-		return deck.size();
-	}
+    public int size() {
+        return deck.size();
+    }
 
-	public void buildCards() {
-		deck = new LinkedList<Card>();
+    public void buildCards() {
+        deck = new LinkedList<Card>();
+    // Добавляю карты в колоду.
+        for (int i = 0; i < CardsSuit.values().length; i++) {
+			for (int j=0;j<CardsRang.values().length;j++){
+                deck.add(new Card(game, false, CardsSuit.values()[i], CardsRang.values()[j], x, y));
+            }
+        }
 
-		// Clubs
-		deck.add(new Card(game,false,Card.CLUBS, Card.SIX, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.SEVEN, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.EIGHT, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.NINE, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.TEN, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.JACK, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.QUEEN, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.KING, x, y));
-		deck.add(new Card(game,false,Card.CLUBS, Card.ACE, x, y));
+//        // Clubs
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.SIX, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.SEVEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.EIGHT, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.NINE, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.TEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.JACK, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.QUEEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.KING, x, y));
+//        deck.add(new Card(game, false, CardsSuit.CLUBS, CardsRang.ACE, x, y));
+//
+//        // Spades
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.SIX, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.SEVEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.EIGHT, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.NINE, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.TEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.JACK, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.QUEEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.KING, x, y));
+//        deck.add(new Card(game, false, CardsSuit.SPADES, CardsRang.ACE, x, y));
+//
+//        // Hearts
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.SIX, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.SEVEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.EIGHT, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.NINE, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.TEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.JACK, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.QUEEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.KING, x, y));
+//        deck.add(new Card(game, false, CardsSuit.HEARTS, CardsRang.ACE, x, y));
+//
+//        // Diamonds
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.SIX, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.SEVEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.EIGHT, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.NINE, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.TEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.JACK, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.QUEEN, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.KING, x, y));
+//        deck.add(new Card(game, false, CardsSuit.DIAMONDS, CardsRang.ACE, x, y));
 
-		// Spades
-		deck.add(new Card(game,false,Card.SPADES, Card.SIX, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.SEVEN, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.EIGHT, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.NINE, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.TEN, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.JACK, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.QUEEN, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.KING, x, y));
-		deck.add(new Card(game,false,Card.SPADES, Card.ACE, x, y));
+    }
 
-		// Hearts
-		deck.add(new Card(game,false,Card.HEARTS, Card.SIX, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.SEVEN, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.EIGHT, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.NINE, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.TEN, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.JACK, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.QUEEN, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.KING, x, y));
-		deck.add(new Card(game,false,Card.HEARTS, Card.ACE, x, y));
+    public LinkedList<Card> getDeck() {
 
-		// Diamonds
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.SIX, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.SEVEN, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.EIGHT, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.NINE, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.TEN, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.JACK, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.QUEEN, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.KING, x, y));
-		deck.add(new Card(game,false,Card.DIAMONDS, Card.ACE, x, y));
+        return deck;
 
-	}
+    }
 
-	public LinkedList<Card> getDeck() {
+    @Override
+    public void tick() {
+        // TODO Auto-generated method stub
 
-		return deck;
+    }
 
-	}
+    @Override
+    public void render(Graphics g) {
+        // TODO Auto-generated method stub
+        int count = 3;
+        for (int i = 0; i < deck.size(); i++) {
 
-	@Override
-	public void tick() {
-		// TODO Auto-generated method stub
+            g.drawImage(deck.get(i).faceDoWn(), 80 - count - i, 350, null);
+            count++;
 
-	}
+        }
+    }
 
-	@Override
-	public void render(Graphics g) {
-		// TODO Auto-generated method stub
-		int count =3;
-		for(int i=0;i<deck.size();i++) {
-			
-			g.drawImage(deck.get(i).faceDoWn(), 80-count-i, 350, null);
-			count++;
-			
-		}
-	}
 
 }
